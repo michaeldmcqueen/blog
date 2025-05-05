902cbed1707c5588140551371133cc998c51c220
Michael McQueen <michaelmcqueen@Michaels-MacBook-Pro-2.local>
2025-05-05 16:37:11 -0600

    Install 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Home of Michael McQueen",
+	url: "https://michaeldmcqueen.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Michael McQueen",
+		email: "michaeldmcqueen@gmail.com",
+		url: "https://michaeldmcqueen.com/about/"
+	}
+}
